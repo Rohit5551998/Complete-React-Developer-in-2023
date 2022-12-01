8dd466953b1873774ebfa25df2b3f1600af48d8b
Rohit Chawhan <rohitchawhan555@gmail.com>
2022-12-01 10:04:01 +0530

    Mapping Array to Elements

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -6,34 +6,56 @@ class App extends Component {
   constructor() {
     super();
 
+    // this.state = {
+    //   name: { firstName: 'Rohit', lastName: 'Chawhan' },
+    //   academy: 'ZTM',
+    // };
+
     this.state = {
-      name: { firstName: 'Rohit', lastName: 'Chawhan' },
-      academy: 'ZTM',
-    };
+      monsters: [
+        {
+          name: 'Linda',
+        },
+        {
+          name: 'Frank',
+        },
+        {
+          name: 'Jacky',
+        }
+      ]
+    }
   }
 
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Hi {this.state.name.firstName} {this.state.name.lastName},
-            I learn at {this.state.academy}.
-          </p>
-          <button onClick={() => {
-            // this.setState({ name: { firstName: 'Yihua', lastName: 'Zhang' } });
-            // Ensuring console.log runs after state is updated through secondary callback
-            this.setState(() => {
-              return {
-                name: { firstName: 'Yihua', lastName: 'Zhang' }
-              }
-            },
-              () => {
-                console.log(this.state);
-              });
-          }}>Change Name</button>
-        </header>
+      // <div className="App">
+      //   <header className="App-header">
+      //     <img src={logo} className="App-logo" alt="logo" />
+      //     <p>
+      //       Hi {this.state.name.firstName} {this.state.name.lastName},
+      //       I learn at {this.state.academy}.
+      //     </p>
+      //     <button onClick={() => {
+      //       // this.setState({ name: { firstName: 'Yihua', lastName: 'Zhang' } });
+      //       // Ensuring console.log runs after state is updated through secondary callback
+      //       this.setState(() => {
+      //         return {
+      //           name: { firstName: 'Yihua', lastName: 'Zhang' }
+      //         }
+      //       },
+      //         () => {
+      //           console.log(this.state);
+      //         });
+      //     }}>Change Name</button>
+      //   </header>
+      // </div>
+
+      <div className='App'>
+        {
+          this.state.monsters.map((monster) => {
+            return <h1>{monster.name}</h1>;
+          })
+        }
       </div>
     );
   }
