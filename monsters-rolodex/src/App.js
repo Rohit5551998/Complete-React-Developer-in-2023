b5600ee3f84b36cc045f5142ce9e90c4dd0b1cfd
Rohit Chawhan <rohitchawhan555@gmail.com>
2022-12-05 15:32:16 +0530

    Storing Original Data and Optimizations

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -27,64 +27,80 @@ class App extends Component {
       // }
       // ]
       monsters: [],
-    }
+      searchField: '',
+    };
+    console.log('Constructor');
   }
 
   componentDidMount() {
     fetch('https://jsonplaceholder.typicode.com/users')
       .then((response) => response.json())
-      .then((users) => this.setState(() => {
-        return { monsters: users }
-      }, () => {
-        console.log(this.state);
-      }));
+      .then((users) => this.setState(
+        () => {
+          return { monsters: users }
+        },
+        () => {
+          console.log(this.state);
+        }
+      )
+      );
+  }
+
+  // render() {
+  // return (
+  // <div className="App">
+  //   <header className="App-header">
+  //     <img src={logo} className="App-logo" alt="logo" />
+  //     <p>
+  //       Hi {this.state.name.firstName} {this.state.name.lastName},
+  //       I learn at {this.state.academy}.
+  //     </p>
+  //     <button onClick={() => {
+  //       // this.setState({ name: { firstName: 'Yihua', lastName: 'Zhang' } });
+  //       // Ensuring console.log runs after state is updated through secondary callback
+  //       this.setState(() => {
+  //         return {
+  //           name: { firstName: 'Yihua', lastName: 'Zhang' }
+  //         }
+  //       },
+  //         () => {
+  //           console.log(this.state);
+  //         });
+  //     }}>Change Name</button>
+  //   </header>
+  // </div>
+  // );
+  // }
+  onSearchChange = (event) => {
+    console.log(event.target.value);
+    const searchField = event.target.value.toLowerCase();
+    this.setState(() => {
+      return { searchField };
+    })
   }
 
   render() {
-    // return (
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Hi {this.state.name.firstName} {this.state.name.lastName},
-    //       I learn at {this.state.academy}.
-    //     </p>
-    //     <button onClick={() => {
-    //       // this.setState({ name: { firstName: 'Yihua', lastName: 'Zhang' } });
-    //       // Ensuring console.log runs after state is updated through secondary callback
-    //       this.setState(() => {
-    //         return {
-    //           name: { firstName: 'Yihua', lastName: 'Zhang' }
-    //         }
-    //       },
-    //         () => {
-    //           console.log(this.state);
-    //         });
-    //     }}>Change Name</button>
-    //   </header>
-    // </div>
-    // );
+    console.log('render');
+
+    const { monsters, searchField } = this.state;
+    const { onSearchChange } = this;
+
+    const filteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLowerCase().includes(searchField);
+    })
+
     return (
-      <div className='App'>
+      <div className='App' >
         <input className='search-box' type='search' placeholder='Search Monsters'
-          onChange={(event) => {
-            console.log(event.target.value);
-            const searchString = event.target.value.toLowerCase();
-            const filteredMonsters = this.state.monsters.filter((monster) => {
-              return monster.name.toLowerCase().includes(searchString);
-            })
-            this.setState(() => {
-              return { monsters: filteredMonsters };
-            })
-          }} />
+          onChange={onSearchChange} />
         {
-          this.state.monsters.map((monster) => {
+          filteredMonsters.map((monster) => {
             return <div key={monster.id}>
               <h1>{monster.name}</h1>
             </div>;
           })
         }
-      </div>
+      </div >
     );
   }
 }
