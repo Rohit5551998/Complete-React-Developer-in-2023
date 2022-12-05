0d4d610e8df3ff84b7a15951d2a844479025ed3d
Rohit Chawhan <rohitchawhan555@gmail.com>
2022-12-05 16:07:51 +0530

    Adding Card List Component Template

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -1,6 +1,6 @@
 import { Component } from 'react';
 import './App.css';
-import logo from './logo.svg';
+import CardList from './components/card-list/card-list.component';
 
 class App extends Component {
   constructor() {
@@ -93,13 +93,14 @@ class App extends Component {
       <div className='App' >
         <input className='search-box' type='search' placeholder='Search Monsters'
           onChange={onSearchChange} />
-        {
+        {/* {
           filteredMonsters.map((monster) => {
             return <div key={monster.id}>
               <h1>{monster.name}</h1>
             </div>;
           })
-        }
+        } */}
+        <CardList />
       </div >
     );
   }
---
+++ monsters-rolodex/src/components/card-list/card-list.component.jsx
@@ -0,0 +1,14 @@
+import { Component } from "react";
+
+class CardList extends Component {
+
+  render() {
+    return (
+      <div>
+        This is the CardList Component
+      </div>
+    );
+  }
+}
+
+export default CardList;
