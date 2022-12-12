ff90a5c6104fe9dd23635512587879bc676b94b9
Rohit Chawhan <rohitchawhan555@gmail.com>
2022-12-12 14:48:03 +0530

    Firestore and Users Document Setup

--- crown-clothing/src/routes/sign-in/sign-in.component.jsx
+++ crown-clothing/src/routes/sign-in/sign-in.component.jsx
@@ -1,7 +1,20 @@
+import {
+  signInWithGooglePopUp, createUserDocumentFromAuth
+} from "../../utils/firebase/firebase.util";
+
 const SignIn = () => {
+  const logGoogleUser = async () => {
+    // const response = await signInWithGooglePopUp();
+    const { user } = await signInWithGooglePopUp();
+    createUserDocumentFromAuth(user);
+  }
+
   return (
     <div>
       <h1>Sign In Page</h1>
+      <button onClick={logGoogleUser}>
+        Sign In with Google Popup
+      </button>
     </div>
   );
 }
---
+++ crown-clothing/src/utils/firebase/firebase.util.js
@@ -0,0 +1,38 @@
+// Import the functions you need from the SDKs you need
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { doc, getDoc, getFirestore } from "firebase/firestore";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAOJdv3LVdqwY2Tprc5cPcvzpINSUOvPQM",
+  authDomain: "crwn-db-72891.firebaseapp.com",
+  projectId: "crwn-db-72891",
+  storageBucket: "crwn-db-72891.appspot.com",
+  messagingSenderId: "809238776406",
+  appId: "1:809238776406:web:428b93ae6ac8c73e0fc9f7"
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+
+provider.setCustomParameters({
+  prompt: "select_account",
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopUp = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+  const userDocRef = doc(db, 'users', userAuth.uid);
+
+  console.log(userDocRef);
+
+  const userSnapshot = await getDoc(userDocRef);
+}
