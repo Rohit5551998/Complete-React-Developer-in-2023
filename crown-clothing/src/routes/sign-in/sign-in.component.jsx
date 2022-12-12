a35cf4a590b9f8d8972705de2796ce56ccdca8bf
Rohit Chawhan <rohitchawhan555@gmail.com>
2022-12-12 15:14:06 +0530

    Sign In With Redirect

--- crown-clothing/src/routes/sign-in/sign-in.component.jsx
+++ crown-clothing/src/routes/sign-in/sign-in.component.jsx
@@ -1,13 +1,31 @@
+import { useEffect } from "react";
+import { getRedirectResult } from "firebase/auth";
 import {
-  signInWithGooglePopUp, createUserDocumentFromAuth
+  auth,
+  signInWithGooglePopUp,
+  // signInWithGoogleRedirect,
+  createUserDocumentFromAuth,
 } from "../../utils/firebase/firebase.util";
 
 const SignIn = () => {
+
+  // useEffect(() => async () => {
+  //   const response = await getRedirectResult(auth);
+
+  //   if (response) {
+  //     const userDocRef = await createUserDocumentFromAuth(response.user);
+  //   }
+
+  // }, []);
+
   const logGoogleUser = async () => {
     // const response = await signInWithGooglePopUp();
     const { user } = await signInWithGooglePopUp();
     const userDocRef = await createUserDocumentFromAuth(user);
   }
+  // const logGoogleRedirectUser = async () => {
+  //   const { user } = await signInWithGoogleRedirect();
+  // }
 
   return (
     <div>
@@ -15,6 +33,9 @@ const SignIn = () => {
       <button onClick={logGoogleUser}>
         Sign In with Google Popup
       </button>
+      {/* <button onClick={logGoogleRedirectUser}>
+        Sign In with Google Redirect
+      </button> */}
     </div>
   );
 }
--- crown-clothing/src/utils/firebase/firebase.util.js
+++ crown-clothing/src/utils/firebase/firebase.util.js
@@ -2,7 +2,7 @@
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 import { initializeApp } from "firebase/app";
-import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { getAuth, GoogleAuthProvider, signInWithPopup, signInWithRedirect } from "firebase/auth";
 import { doc, getDoc, getFirestore, setDoc } from "firebase/firestore";
 
 // Your web app's Firebase configuration
@@ -18,14 +18,15 @@ const firebaseConfig = {
 // Initialize Firebase
 const firebaseApp = initializeApp(firebaseConfig);
 
-const provider = new GoogleAuthProvider();
+const googleProvider = new GoogleAuthProvider();
 
-provider.setCustomParameters({
+googleProvider.setCustomParameters({
   prompt: "select_account",
 });
 
 export const auth = getAuth();
-export const signInWithGooglePopUp = () => signInWithPopup(auth, provider);
+export const signInWithGooglePopUp = () => signInWithPopup(auth, googleProvider);
+export const signInWithGoogleRedirect = () => signInWithRedirect(auth, googleProvider);
 
 export const db = getFirestore();
 
